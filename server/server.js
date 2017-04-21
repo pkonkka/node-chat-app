09ce5bf6cbfa2183e4b6df2185433ae1cc84ed76
Petri Konkka <petrikonkka@gmail.com>
2017-04-21 12:44:20 +0300

    Port: OR flag corrected

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const http = require('http');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
-// const port = process.env.PORT | 3000;
-const port = process.env.PORT;
-console.log('port:', port);
+const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer((app));
