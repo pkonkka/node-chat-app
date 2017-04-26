02ca1c99d7d92de3dd44fb61d9c482b09e2dac9b
Petri Konkka <petrikonkka@gmail.com>
2017-04-26 10:22:03 +0300

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessagHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessagHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');    
 });
@@ -19,7 +35,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,6 +48,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);    
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
